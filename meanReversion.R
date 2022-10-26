76f13bd4516ddd45527bb1d7914b16cb6f9d2e4e
Yujing Chen <99555856+AnnieCHENCHEN@users.noreply.github.com>
2022-10-26 17:11:55 +0100

    Update meanReversion.R

--- meanReversion.R
+++ meanReversion.R
@@ -1,7 +1,11 @@
-
+# Mean reversion strategy (with BBands with proit target)
+# This strategy uses only marker orders
 
 maxRows <- 3100 # used to initialize a matrix to store closing prices
 # set maxRows as the number of rows in data (it can be larger but should not be smaller)
+
+library(xts)
+library(TTR)
 
 getOrders <- function(store, newRowList, currentPos, info, params) {
   
@@ -10,14 +14,16 @@ getOrders <- function(store, newRowList, currentPos, info, params) {
   if (is.null(store)) store <- initStore(newRowList,params$series)
   store <- updateStore(store, newRowList, params$series)
   
-  marketOrders <- -currentPos; pos <- allzero
+  marketOrders <- -currentPos #close all positions
+  pos <- allzero #set pos as a zero vector
+  profit <- 0 #inital profit=0
+  
+  
   
   if (store$iter > params$lookback) {
-    startIndex <-  store$iter - params$lookback
+    startIndex <-  store$iter - params$lookback 
     for (i in 1:length(params$series)) {
       cl <- newRowList[[params$series[i]]]$Close
-      vl <- newRowList[[params$series[i]]]$Volume #? need to discuss
-      
       bbands <- last(BBands(store$cl[startIndex:store$iter,i],
                             n=params$lookback,sd=params$sdParam))
       if (cl < bbands[,"dn"]) {
@@ -28,14 +34,31 @@ getOrders <- function(store, newRowList, currentPos, info, params) {
         # if close is relatively high go short (again, contrarian type)
         pos[params$series[i]] <- -params$posSizes[params$series[i]]
       }
-    }
+    
+      # we want to sum up the profit (1 to i) of the whole trading, we use this profit to compare with our profit target 
+      # which is profit target =50000
+      profit <- sapply(info$netWorth[i], sum, simplify = TRUE) 
+      
+      # profit < profit target, we will contiune this strategy and this "if" command
+      if (profit < params$profit_target) {
+          next
+      }
+      # profit >= params$profit_target, we will exit the market and stop trading
+        else
+          pos[params$series[i]] <- 0  #update current position
+          break
+      }
+
   }
+
   marketOrders <- marketOrders + pos
+  
   return(list(store=store,marketOrders=marketOrders,
               limitOrders1=allzero,limitPrices1=allzero,
               limitOrders2=allzero,limitPrices2=allzero))
+
 }
-#for close price 
+
 initClStore  <- function(newRowList,series) {
   clStore <- matrix(0,nrow=maxRows,ncol=length(series))
   return(clStore)
@@ -45,24 +68,11 @@ updateClStore <- function(clStore, newRowList, series, iter) {
     clStore[iter,i] <- as.numeric(newRowList[[series[i]]]$Close)
   return(clStore)
 }
-#for profit
-initStore_Profit <- function(newRowList,series) {
-  profit_store <- matrix(0,nrow=maxRows,ncol=length(series))
-  return(profit_store)
-}
-updateStore_Profit <- function(profit_store, newRowList, series, iter) {
-  for (i in 1:length(series))
-    profit_store[iter,i] <- as.numeric(newRowList[[series[i]]]$Volume)
-                              *as.numeric(newRowList[[series[i]]]$Close)
-  return(profit_store)
-}
-#store = iter and close price and profit
 initStore <- function(newRowList,series) {
-  return(list(iter=0,cl=initClStore(newRowList,series), profit=initStore_Profit(newRowList,series)))
+  return(list(iter=0,cl=initClStore(newRowList,series)))
 }
 updateStore <- function(store, newRowList, series) {
   store$iter <- store$iter + 1
-  store$cl <- updateClStore(store$cl,newRowList,series,store$iter)
-  store$profit <- updateStore_Profit(store$profit,newRowList,series,store$iter)
+  store$cl <- updateClStore(store$cl,newRowList,series,store$iter) 
   return(store)
 }
