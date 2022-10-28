58f5562dc0b69f160ac3f4eee7f798157fa7db53
Yujing Chen <99555856+AnnieCHENCHEN@users.noreply.github.com>
2022-10-28 21:00:42 +0100

    Update momentum.R

--- momentum.R
+++ momentum.R
@@ -1,54 +1,48 @@
+# Momentum strategy (with TMA with traling stop loss)
+# This strategy uses only marker order
 
 getOrders <- function(store, newRowList, currentPos, info, params) {
-  
-  ###########################################################################
-  # You do not need to edit this next part of the code
-  ###########################################################################
   allzero  <- rep(0,length(newRowList)) # used for initializing vectors
   pos <- allzero
+  stop_loss =0 #set stop loss=0
   
   if (is.null(store)) 
     store <- initStore(newRowList)  
   else
     store <- updateStore(store, newRowList)  
-  ###########################################################################
   
-  ###########################################################################
-  # This next code section is the only one you
-  # need to edit for getOrders
-  #
-  # The if condition is already correct:
-  # you should only start computing the moving 
-  # averages when you have enough (close) prices 
-  # for the long moving average   
-  ###########################################################################
+
   if (store$iter > params$lookbacks$long) {   
-    # ENTER STRATEGY LOGIC HERE
-    
-    # remember to only consider the series in params$series
-    
-    # You will need to get the current_close
-    # either from newRowList or from store$cl
-    
-    # You will also need to get prices 
-    # from store$cl
-    
-    # With these you can use getTMA, getPosSignFromTMA
-    # and getPosSize to assign positions to the vector pos
     for(i in params$series){
       if(i != 0){
         TMA_List <- getTMA(store$cl[[i]], params$lookbacks)  #return a list
         CurrentClose <- last(store$cl[[i]])
         pos[i] <- getPosSignFromTMA(TMA_List) * getPosSize(CurrentClose)
+      
+      #traling stop loss
+      #long，close price + trailing stop loss (100) = current close price
+      # next trade close rice <= current close price，exit the makret (pos <-0),otherwise continue to trade
+      if (pos[i] ==1){
+        highestPrice = max(store$cl[[1:i]])
+        maxStopLoss_price = (1-0.1) * highestPrice
+        
+        if (store$cl[[i]] <= maxStopLoss_price){ #
+          pos[i] <- 0
+          }else{ #store$cl[[i]] > maxStopLoss_price
+            next #continue stay in a trade
+          }
+        
       }
+        
+      else if (pos[i] == -1){
+        
+        
+      }
+      }
+      
     }
   }
-  #Edit tailing stop loss
-  ###########################################################################
-  
-  ###########################################################################
-  # You do not need to edit the rest of this function
-  ###########################################################################
+
   marketOrders <- -currentPos + pos
   
   return(list(store=store,marketOrders=marketOrders,
@@ -58,13 +52,6 @@ getOrders <- function(store, newRowList, currentPos, info, params) {
 
 getTMA <- function(prices, lookbacks) {
   
-  # prices and lookbacks should pass (return FALSE) when used with
-  # the 6 checks, as tested in the following call to allChecks that 
-  # you should not edit
-  if (with_checks)
-    if (atLeastOneError(close_prices, lookbacks))
-      stop('At least one of the errors E01...E06 occured')
-  
   ret <- 0
   
   sma <- 0
@@ -73,29 +60,11 @@ getTMA <- function(prices, lookbacks) {
     sma[i] <- as.numeric(last(SMA(prices$Close, n = lookbacks[[i]])))
     ret[[i]] <- sma[i]
   }
-  # You need to replace the assignment to ret so that the returned object:
-  #    - is a list 
-  #    - has the right names (short, medium, long), and
-  #    - contains numeric and not xts objects
-  #    - and contains the correct moving average values, which should 
-  #      have windows of the correct sizes that all end in the 
-  #      same period, be the last row of prices
   return(ret)
 }
 
 getPosSignFromTMA <- function(tma_list) {
-  # This function takes a list of numbers tma_list with three elements 
-  # called short, medium, and long, which correspond to the SMA values for 
-  # a short, medium and long lookback, respectively.
-  
-  # Note that if both this function and getTMA are correctly implemented 
-  # then the following should work with correct input arguments:
-  # getPositionFromTMA(getTMA(prices,lookbacks))
-  
-  # This function should return a single number that is:
-  #       -1 if the short SMA < medium SMA < long SMA
-  #        1 if the short SMA > medium SMA > long SMA
-  #        0 otherwise
+
   if(tma_list[[1]] < tma_list[[2]] && tma_list[[2]] < tma_list[[3]])
     return(-1)
   else if(tma_list[[1]] > tma_list[[2]] && tma_list[[2]] > tma_list[[3]])
@@ -105,15 +74,11 @@ getPosSignFromTMA <- function(tma_list) {
 }
 
 getPosSize <- function(current_close,constant=5000) { 
-  # This function should return (constant divided by current_close) 
-  # rounded down to the nearest integer
   RoundDown_int <- floor(constant/current_close)
   return(RoundDown_int)
 }
 
-###############################################################################
-# The functions below do NOT need to be edited
-###############################################################################
+
 initClStore  <- function(newRowList) {
   clStore <- lapply(newRowList, function(x) x$Close)  
   return(clStore)
