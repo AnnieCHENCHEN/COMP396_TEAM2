55497aa93aecf37dbbac746c5a4376f281112fb3
fukisang <80524515+fukisang@users.noreply.github.com>
2022-11-16 16:49:42 +0000

    Update op_rsi.R

--- op_rsi.R
+++ op_rsi.R
@@ -7,10 +7,10 @@ source('strategies/rsi_contrarian.R')
 #split data into 3 parts
 training_days <- 500  
 validation_days <- 250  
-testing_days <- 250
+testing_days <- 350
 ##########################################################################
 #training days
-numOfDays <- training_days  
+numOfDays <- training_days
 dataList <- getData(directory="PART1")
 dataList <- lapply(dataList, function(x) x[1:numOfDays])
 ########################################################################
@@ -22,7 +22,7 @@ dataList <- lapply(dataList, function(x) x[1:numOfDays])
 #######################################################################
 #test
 # StartDay <- 751
-# EndDay <- validation_days+250
+# EndDay <- validation_days+350
 # dataList <- getData(directory="PART1")
 # dataList <- lapply(dataList, function(x) x[StartDay:EndDay])
 ######################################################################
@@ -31,11 +31,14 @@ sMult <- 0.2 # slippage multiplier
 #sMult <- 0
 lookbackSeq <- c(5,15,25,35,45,55,65,75,85,95,105,115)
 threshold <- c(5,10,15,20)
-P_size <- list(c(136,4860,11,123,21,21066,1,1652,6,95),
-               c(811,28972,66,733,125,125581,6,9848,36,566),
-               c(272,9541,22,248,44,41488,2,3346,14,180))
-
-params_comb <- expand.grid(lookback=lookbackSeq,threshold=threshold, pSize=P_size)
+P_size <- list(c(464,15544,40,391,51,10663,1,5715,17,106),
+               c(136,4860,11,123,21,21066,1,1652,6,95))
+#out-sample
+# lookbackSeq <- 35
+# threshold <- 20
+# P_size <- list(c(136,4860,11,123,21,21066,1,1652,6,95),
+#                c(811,28972,66,733,125,125581,6,9848,36,566))
+# params_comb <- expand.grid(lookback=lookbackSeq,threshold=threshold, pSize=P_size)
 
 resultsMatrix <- matrix(nrow=nrow(params_comb),ncol=4)
 colnames(resultsMatrix) <- c("lookback","threshold","posSize","PD Ratio")
@@ -45,22 +48,27 @@ print(nrow(params_comb))
 for (i in 1:nrow(params_comb)) {
   params <- list(lookback=params_comb$lookback[[i]],threshold=params_comb$threshold[[i]],
                  series=1:10,posSizes=params_comb$pSize[[i]]) 
+  print(as.numeric(params_comb$pSize[[i]]))
+  print(as.numeric(P_size[[i]]))
   results <- backtest(dataList, getOrders, params, sMult)
   pfolioPnL <- plotResults(dataList,results)
-  if(params_comb$pSize[[i]] == P_size[[1]]){
+  if(all(params_comb$pSize[[i]] == P_size[[1]]) == TRUE){
     resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$threshold[[i]],
-                           "close_AADS",pfolioPnL$fitAgg)
+                           "close",pfolioPnL$fitAgg)
   }
-  if(params_comb$pSize[[i]] == P_size[[2]]){
+  if(all(params_comb$pSize[[i]] == P_size[[2]]) == TRUE){
     resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$threshold[[i]],
-                           "close_ST",pfolioPnL$fitAgg)
+                           "AADS",pfolioPnL$fitAgg)
   }
-  if(params_comb$pSize[[i]] == P_size[[3]]){
-    resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$threshold[[i]],
-                           "open_ST",pfolioPnL$fitAgg)
-  }
+  # if(params_comb$pSize[[i]] == P_size[[3]]){
+  #   resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$threshold[[i]],
+  #                          "open_ST",pfolioPnL$fitAgg)
+  # }
+  # resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$threshold[[i]],
+  #                        "close_AADS",pfolioPnL$fitAgg)
   pfolioPnLList[[i]]<- pfolioPnL
   cat("Just completed",i,"out of",nrow(params_comb),"\n")
   print(resultsMatrix[i,])
 }
 print(resultsMatrix[order(resultsMatrix[,"PD Ratio"],decreasing=TRUE),])
+
