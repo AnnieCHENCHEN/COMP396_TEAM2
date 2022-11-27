bdafb5ad5ad8fff693560ddc608330f544ea7f32
fukisang <80524515+fukisang@users.noreply.github.com>
2022-11-27 17:45:03 +0000

    Update optimize_meanReversion.R

--- optimize_meanReversion.R
+++ optimize_meanReversion.R
@@ -5,35 +5,30 @@ source('strategies/meanReversion.R')
 
 #training dyas = 500  validation days = 250  testing days = 250
 #split data into 3 parts
-training_days <- 500  
-validation_days <- 250  
+training_days <- 550  
 testing_days <- 250
 ##########################################################################
 #training days
-numOfDays <- training_days  
-dataList <- getData(directory="PART1")
-dataList <- lapply(dataList, function(x) x[1:numOfDays])
-########################################################################
-#validation 
-# StartDay <- training_days+1
-# EndDay <- training_days+250
+# numOfDays <- training_days  
 # dataList <- getData(directory="PART1")
-# dataList <- lapply(dataList, function(x) x[StartDay:EndDay])
+# dataList <- lapply(dataList, function(x) x[1:numOfDays])
 #######################################################################
 #test
-# StartDay <- 751
-# EndDay <- validation_days+250
-# dataList <- getData(directory="PART1")
-# dataList <- lapply(dataList, function(x) x[StartDay:EndDay])
+StartDay <- training_days + 1
+EndDay <- 1100
+dataList <- getData(directory="PART1")
+dataList <- lapply(dataList, function(x) x[StartDay:EndDay])
 ######################################################################
 sMult <- 0.2 # slippage multiplier
-
-lookbackSeq <- c(5,15,25,35,45,55,65,75,85,95,105,115)
-sdParam <- c(0.5,1,1.5,2)
-P_size <- list(c(136,4860,11,123,21,21066,1,1652,6,95),
-               c(811,28972,66,733,125,125581,6,9848,36,566),
-               c(136,4773,11,124,22,20754,1,1674,7,90),
-               c(272,9541,22,248,44,41488,2,3346,14,180))
+# in-sample parameters
+# lookbackSeq <- c(5,15,25,35,45,55,65,75,85,95,105,115)
+# sdParam <- c(0.5,1,1.5,2)
+# P_size <- list(c(464,15544,40,391,51,10663,1,5715,17,106),
+#                c(136,4860,11,123,21,21066,1,1652,6,95))
+#out-sample parameters
+lookbackSeq <- c(45)
+sdParam <- c(2)
+P_size <- list(c(136,4860,11,123,21,21066,1,1652,6,95))
 
 params_comb <- expand.grid(lookback=lookbackSeq,sd=sdParam, pSize=P_size)
 
@@ -47,21 +42,16 @@ for (i in 1:nrow(params_comb)) {
                    series=1:10,posSizes=params_comb$pSize[[i]]) 
     results <- backtest(dataList, getOrders, params, sMult)
     pfolioPnL <- plotResults(dataList,results)
-    if(params_comb$pSize[[i]] == c(136,4860,11,123,21,21066,1,1652,6,95)){
+    if(all(params_comb$pSize[[i]] == P_size[[1]]) == TRUE){
       resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$sd[[i]],
-                             "close_AADS",pfolioPnL$fitAgg)
-    }else if(params_comb$pSize[[i]] == c(811,28972,66,733,125,125581,6,9848,36,566)){
+                             "AADS",pfolioPnL$fitAgg)
+    }
+    if(all(params_comb$pSize[[i]] == P_size[[2]]) == TRUE){
       resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$sd[[i]],
-                             "close_ST",pfolioPnL$fitAgg)
-    }else if(params_comb$pSize[[i]] == c(136,4773,11,124,22,20754,1,1674,7,90)){
-      resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$sd[[i]],
-                             "open_AADS",pfolioPnL$fitAgg)
-    }else{
-      resultsMatrix[i,] <- c(params_comb$lookback[[i]],params_comb$sd[[i]],
-                             "open_ST",pfolioPnL$fitAgg)
+                             "AADS",pfolioPnL$fitAgg)
     }
     pfolioPnLList[[i]]<- pfolioPnL
     cat("Just completed",i,"out of",nrow(params_comb),"\n")
     print(resultsMatrix[i,])
 }
-print(resultsMatrix[order(resultsMatrix[,"PD Ratio"]),])
+print(resultsMatrix[order(resultsMatrix[,"PD Ratio"],decreasing = TRUE),])
