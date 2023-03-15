9f3b2cf8e421e7a17810f86601b00043d2588265
Yujing Chen <99555856+AnnieCHENCHEN@users.noreply.github.com>
2023-03-15 23:33:47 +0000

    Update optimize_turtle.R

--- ass2_op/optimize_turtle.R
+++ ass2_op/optimize_turtle.R
@@ -3,7 +3,7 @@ source('framework/backtester.R')
 source('framework/processResults.R'); 
 source('strategies/turtle_trade.R') 
 
-#sink("ass2_op/opti_Turtle.txt")
+sink("optim/opti_Turtle.txt")
 
 #training days = 500  validation days = 250  testing days = 250
 #split data into 3 parts
@@ -23,16 +23,16 @@ dataList <- lapply(dataList, function(x) x[1:numOfDays])
 ######################################################################
 sMult <- 0.2 # slippage multiplier
 # in-sample parameters
-periods_short <- seq(from=10, to=40, by=10)
-periods_med <- seq(from=20,to=40,by=10)
-periods_long <- seq(from=45, to=65,by=10)
-Multi_N <- seq(from=5, to=7, by=1)
-multi <- seq(from=3, to=5, by=1)
-capi_Ratio <- seq(from=0.1,to=0.5,by=0.2)
-moneyRatio =seq(from=0.02,to=0.05,by=0.01)
+periods_short <- seq(from=10, to=50, by=5)
+periods_med <- seq(from=15,to=60,by=5)
+periods_long <- seq(from=40, to=90,by=5)
+Multi_N <- seq(from=5, to=10, by=1)
+multi <- seq(from=3, to=5, by=0.5)
+capi_Ratio <- seq(from=0.3,to=0.7,by=0.1)
 spreadPercentage=0.001
-Units <- 0
-size<- 0.01
+moneyRatio =seq(from=0.02,to=0.05,by=0.01)
+series_com <- t(combn(1:10,4)) #randomly pick 4 series as a group to optimize
+
 #out-sample parameters
 #lookbackSeq
 #multiple
@@ -43,24 +43,29 @@ size<- 0.01
 
 params_comb <- expand.grid(ex_1=periods_short, en_1=periods_med,
                            en_2=periods_long,mul=Multi_N, Ratio=moneyRatio,mutiple=multi,
-                           spread=spreadPercentage,money=capi_Ratio, units=Units)
+                           spread=spreadPercentage,money=capi_Ratio)
+# Create a data frame for the series combinations
+series_df <- as.data.frame(series_com)
+colnames(series_df) <- paste0("series_", 1:4)
 
-resultsMatrix <- matrix(nrow=nrow(params_comb),ncol=8)
-colnames(resultsMatrix) <- c("Ex_1","En_1","En_2","mult_N","moneyRatio","multi","captialRatio","PD Ratio")
+# Combine the parameter grid with the series combinations
+params_comb <- merge(params_comb, series_df, all=TRUE)
+
+resultsMatrix <- matrix(nrow=nrow(params_comb),ncol=12)
+colnames(resultsMatrix) <- c("Ex_1","En_1","En_2","mult_N","moneyRatio","multi","captialRatio","series_use1","series_use2","series_use3","series_use4","PD Ratio")
 pfolioPnLList <- vector(mode="list",length=nrow(params_comb)) 
 print(nrow(params_comb))
 
 for (i in 1:nrow(params_comb)) {
-  params <- list(periods=list(Ex_1=params_comb$ex_1[[i]],En_1=params_comb$en_1[[i]],En_2=params_comb$en_2[[i]]),Multi_N=params_comb$mul[[i]],moneyRatio=params_comb$Ratio[[i]],
-                 multi=params_comb$mutiple[[i]],spreadPercentage=params_comb$spread[[i]],capi_Ratio=params_comb$money[[i]],Units=0,size=0.01,series=1:10) 
- 
+  params <- list(periods=list(Ex_1=params_comb$ex_1[[i]],En_1=params_comb$en_1[[i]],En_2=params_comb$en_2[[i]]),mutilN=params_comb$mul[[i]],moneyR=params_comb$Ratio[[i]],
+                 mutiple=params_comb$mutiple[[i]],spreadPercentage=params_comb$spread[[i]],captial=params_comb$money[[i]],series=as.numeric(params_comb[, paste0("series_", 1:4)][i, ])) 
   results <- backtest(dataList, getOrders, params, sMult)
   pfolioPnL <- plotResults(dataList,results)
   
   # Do backtest
   if(all(params_comb$ex_1[[i]] < params_comb$en_1[[i]] && params_comb$en_1[[i]] <params_comb$en_2[[i]]) == TRUE){
   resultsMatrix[i,] <- c(params_comb$ex_1[[i]],params_comb$en_1[[i]],params_comb$en_2[[i]],params_comb$mul[[i]],params_comb$Ratio[[i]],
-                         params_comb$mutiple[[i]],captial=params_comb$money[[i]],pfolioPnL$fitAgg)
+                         params_comb$mutiple[[i]],captial=params_comb$money[[i]],as.numeric(params_comb[, paste0("series_", 1:4)][i, ]),pfolioPnL$fitAgg)
   
   pfolioPnLList[[i]]<- pfolioPnL
   }
@@ -68,4 +73,4 @@ for (i in 1:nrow(params_comb)) {
   print(resultsMatrix[i,])
 }
 print(resultsMatrix[order(resultsMatrix[,"PD Ratio"],decreasing = TRUE),])
-#sink()
+sink()
